c6879123aac45cc9144ee99f6ad836f9641afeae
Kyle Fang <zhigang1992@gmail.com>
2021-10-09 16:42:35 +0800

    fix: build

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,11 @@ const isNotNamed = (t: typeof babel.types, node: object) => {
     return !node.id;
   }
   return false;
-}
+};
 
-export default ({ types: t }: typeof babel): babel.PluginObj<{hocs?: string[]}> => ({
+export default ({
+  types: t
+}: typeof babel): babel.PluginObj<{ hocs?: string[] }> => ({
   visitor: {
     VariableDeclaration(path, state) {
       const hocs = state.hocs ?? SUPPORTED_HOCS;
@@ -39,32 +41,34 @@ export default ({ types: t }: typeof babel): babel.PluginObj<{hocs?: string[]}> 
 
       declarators.forEach((declarator) => {
         if (
-          t.isIdentifier(declarator.node.id) &&
-          t.isCallExpression(declarator.node.init) &&
-          isNotNamed(t, declarator.node.init.arguments[0]) &&
-          isAnonymousComponent(hocs)(t, declarator.node.init.callee)
+          !t.isIdentifier(declarator.node.id) ||
+          !t.isCallExpression(declarator.node.init)
         ) {
-          declarator.replaceWith(
-            t.variableDeclarator(
-              t.identifier(declarator.node.id.name),
-              t.callExpression(declarator.node.init.callee, [
-                t.functionExpression(
-                  t.identifier(declarator.node.id.name),
-                  declarator.node.init.arguments[0].params,
-                  // is memo((props) => { return *; }) case
-                  t.isBlockStatement(declarator.node.init.arguments[0].body)
-                    ? declarator.node.init.arguments[0].body
-                    : t.blockStatement([
-                        t.returnStatement(
-                          declarator.node.init.arguments[0].body
-                        )
-                      ])
-                ),
-                ...declarator.node.init.arguments.slice(1),
-              ])
-            )
-          );
+          return;
         }
+        const firstArgument = declarator.node.init.arguments[0] as any;
+        if (
+          !isNotNamed(t, firstArgument) ||
+          !isAnonymousComponent(hocs)(t, declarator.node.init.callee)
+        ) {
+          return;
+        }
+        declarator.replaceWith(
+          t.variableDeclarator(
+            t.identifier(declarator.node.id.name),
+            t.callExpression(declarator.node.init.callee, [
+              t.functionExpression(
+                t.identifier(declarator.node.id.name),
+                firstArgument.params,
+                // is memo((props) => { return *; }) case
+                t.isBlockStatement(firstArgument.body)
+                  ? firstArgument.body
+                  : t.blockStatement([t.returnStatement(firstArgument.body)])
+              ),
+              ...declarator.node.init.arguments.slice(1)
+            ])
+          )
+        );
       });
     }
   }
